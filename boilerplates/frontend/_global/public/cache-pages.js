ad56f5c399cc816ae7a2e11702df04e4057f6302
Erfan Gharib <erfangharib5@gmail.com>
2024-06-07 21:32:48 +0330

    chore: update boilerplates

---
+++ boilerplates/frontend/_global/public/cache-pages.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  console.log('install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
---
+++ boilerplates/frontend/_global/tailwind.config.js
@@ -0,0 +1,74 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/*.{tsx,jsx}',
+    './src/pages/*.{tsx,jsx}',
+    './src/components/*.{tsx,jsx}',
+    './src/components/**/*.{tsx,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-900': '#111',
+        'black-800': '#222',
+      }
+    }
+  },
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        '.click-effect': {
+          '@apply active:brightness-[.6] transition-all': {},
+        },
+        '.err': {
+          '@apply text-red-400 text-sm capitalize': {}
+        },
+        '.input, .textarea': {
+          '@apply rounded-lg p-4 outline-none': {},
+        },
+        '.f-center': {
+          '@apply flex items-center justify-center': {},
+        },
+        '.f-start-center': {
+          '@apply flex items-start justify-center': {},
+        },
+        '.f-start': {
+          '@apply flex items-start justify-start': {},
+        },
+        '.f-center-start': {
+          '@apply flex items-center justify-start': {},
+        },
+        '.f-end-start': {
+          '@apply flex items-end justify-start': {},
+        },
+        '.f-start-end': {
+          '@apply flex items-start justify-end': {},
+        },
+        '.f-center-end': {
+          '@apply flex items-center justify-end': {},
+        },
+        '.f-center-between': {
+          '@apply flex items-center justify-between': {},
+        },
+        '.f-start-between': {
+          '@apply flex items-start justify-between': {},
+        },
+        '.ltr': {
+          direction: 'ltr',
+        },
+        '.link': {
+          '@apply underline decoration-dotted text-blue-400 cursor-pointer opacity-70 relative hover:opacity-100 transition-opacity gap-x-2': {},
+        },
+        '.btn': {
+          '@apply f-center text-white': {}
+        },
+        '.btn-tertiary': {
+          '@apply bg-shekaro-blue-500 hover:shadow-shekaro-blue btn': {}
+        },
+        '.btn-secondary': {
+          '@apply bg-shekaro-red-500 hover:bg-shekaro-red-400 btn': {}
+        }
+      })
+    }
+  ]
+}
