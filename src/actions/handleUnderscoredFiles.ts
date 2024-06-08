2192c6c047d4654106ae3a063704c4ba9282c4c8
Erfan Gharib <erfangharib5@gmail.com>
2024-06-08 21:21:06 +0330

    feat: add support for dynamic underscored files

--- src/actions/handleUnderscoredFiles.ts
+++ src/actions/handleUnderscoredFiles.ts
@@ -4,6 +4,8 @@ import fsExtra from 'fs-extra';
 import path from 'path';
 import { BOILERPLATE_DIR_NAME } from "../global.js";
 import { boilerplatesDirPath } from "../index.js";
+import { deletePathPartsFromEnd } from "../utils.js";
+import { pathToFileURL } from "url";
 
 // TODO write description
 const resolveDestPath = (
@@ -42,7 +44,30 @@ export async function handleUnderscoredFiles (
         const _destPath = resolveDestPath(dirPath, tech, destPath, currentAlias);
 
         if(_destPath && path.parse(dirPath).name.startsWith("_")) {
-            fsExtra.copySync(dirPath, _destPath);
+            // filter dynamic files
+            const filterFn = (src: string) => {
+                return !path.parse(src).name.startsWith('d_');
+            }
+            fsExtra.copySync(dirPath, _destPath, { filter: filterFn });
+        }
+        
+        if(_destPath && path.parse(dirPath).name.startsWith("d_") && dirPath.endsWith('.js')) {
+            const dynamicFile = (await import(pathToFileURL(dirPath).href)).default;
+            
+            if(typeof dynamicFile !== 'function')
+                throw new Error(`invalid dynamic file. function must be default exported. \nPath: ${dirPath}\n`);
+
+            const dynamicFileResult = dynamicFile(tech.projectType, tech.techName);
+            
+            if(typeof dynamicFileResult['data'] !== 'string' || !dynamicFileResult['fileType']?.match(/\.([0-9a-z]+)(?:[\?#]|$)/i))
+                throw new Error('invalid dynamic file. return value must be type of { data: string, fileType: `.${string}` }');
+            
+            const newFilePath = path.join(
+                deletePathPartsFromEnd(_destPath, 1), 
+                path.parse(dirPath).name.replace('d_', '') + dynamicFileResult['fileType']
+            );
+            
+            fs.writeFileSync(newFilePath, dynamicFileResult['data']);
         }
 
         if(!fs.statSync(dirPath).isDirectory())
