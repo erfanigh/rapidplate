80909329369f40e8abcacf68cc28d6aae8e1b8a0
Erfan Gharib <erfangharib5@gmail.com>
2024-06-08 15:19:10 +0330

    feat(cli): add support for nodejs app & telegram bot in cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,15 @@
 import inquirer from "inquirer";
 import { isDirExist } from "./utils.js";
+import { invalidCharsRegex } from "./global.js";
 
 export type T_ProjectTechQuestions = {
-    techName: string
+    techName?: string
     projectType: string;
 }
 
 export type T_Cli = {
     mainQuestions: {
         projectName: string;
-        authorName: string;
-        configTerminalKeeper: boolean;
         projectType: string;
     }
     projectTechQuestions: T_ProjectTechQuestions[]
@@ -23,7 +22,13 @@ const enum promptNames {
     CONFIG_TERMINAL_KEEPER = 'configTerminalKeeper',
 }
 
-const invalidCharsRegex = /[<>:"/\\|?*\x00-\x1F]/
+const enum projectTypes {
+    FULLSTACK = 'fullstack',
+    BACKEND = 'backend',
+    FRONTEND = 'frontend',
+    NODEJS_APP = 'nodejs-app',
+    TELEGRAM_BOT = 'telegram-bot',
+}
 
 const backendQuestion = {
     type: "list",
@@ -31,7 +36,7 @@ const backendQuestion = {
     message: "Backend:",
     loop: false,
     choices: [
-        { name: 'Express.js', value: 'express' },
+        { name: ' Express.js', value: 'express' },
     ]
 }
 const frontendQuestion = {
@@ -40,9 +45,9 @@ const frontendQuestion = {
     message: "Frontend:",
     loop: false,
     choices: [
-        { name: 'Next', value: 'next' },
-        { name: 'React', value: 'react' },
-        { name: 'Vue', value: 'vue' },
+        { name: ' Next', value: 'next' },
+        { name: ' React', value: 'react' },
+        { name: ' Vue', value: 'vue' },
         // { name: 'Nuxt', value: 'nuxt' },
     ]
 }
@@ -66,44 +71,37 @@ const runCli = async (): Promise<T_Cli> => {
             }
         },
         {
-            type: "input",
-            name: promptNames.AUTHOR_NAME,
-            message: "Author Name (e.g. github user name):",
-        },
-        {
-            type: "confirm",
-            name: promptNames.CONFIG_TERMINAL_KEEPER,
-            message: "Add Terminal Keeper Config:",
-        },
-        {
             type: "list",
             name: promptNames.PROJECT_TYPE,
             message: "Project Type:",
             loop: false,
             choices: [
-                { name: 'Full Stack (e.g. express & next)', value: 'fullstack' },
-                { name: 'Backend Only', value: 'backend', },
-                { name: 'Frontend Only', value: 'frontend', },
-                { name: 'Nodejs App', value: 'nodejs-app', },
+                { name: ' Full Stack (e.g. express & next)', value: projectTypes.FULLSTACK },
+                { name: ' Backend Only', value: projectTypes.BACKEND },
+                { name: ' Frontend Only', value: projectTypes.FRONTEND },
+                { name: ' Nodejs App', value: projectTypes.NODEJS_APP },
+                { name: ' Telegram Bot (with telegraf.js)', value: projectTypes.TELEGRAM_BOT },
             ]
         },
     ])
 
-    const _projectTechQuestions = {
+    const _projectTechQuestions: { [key in projectTypes]: () => Promise<T_ProjectTechQuestions[]> } = {
         fullstack: async () => {
             return [
                 (await _projectTechQuestions.backend())[0],
                 (await _projectTechQuestions.frontend())[0]
             ];
         },
-        backend: async (): Promise<T_ProjectTechQuestions[]> => [{ 
+        backend: async () => [{ 
             ...await inquirer.prompt([ backendQuestion ]),
-            projectType: 'backend'
+            projectType: projectTypes.BACKEND
         }],
-        frontend: async (): Promise<T_ProjectTechQuestions[]> => [{
+        frontend: async () => [{
             ...await inquirer.prompt([ frontendQuestion ]),
-            projectType: 'frontend'
+            projectType: projectTypes.FRONTEND
         }],
+        'nodejs-app': async () => [{ projectType:  projectTypes.NODEJS_APP }],
+        'telegram-bot': async () => [{ projectType:  projectTypes.TELEGRAM_BOT }],
     }
 
     const projectTechQuestions = await _projectTechQuestions[mainQuestions.projectType]()
