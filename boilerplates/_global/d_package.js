9197a1fd9584ef21e139212f66ee7092815a9b63
Erfan Gharib <erfangharib5@gmail.com>
2024-07-18 10:35:56 +0330

    feat(d_package.js):
    - get latest package version for each dependency
    - refactor code

--- boilerplates/_global/d_package.js
+++ boilerplates/_global/d_package.js
@@ -1,194 +1,296 @@
 //@ts-check
 // package.json file
 // this a dynamic file (started with `d_`) should be runned.
+import fs from 'fs';
+import path from 'path';
 
-let base = {
-    "type": "module",
-    "dependencies": {
-      "dotenv": "^16.3.1"
+let swcPackageList = [
+    "@swc/cli",
+    "@swc/core",
+    "chokidar",
+]
+
+let dependencies = {
+    next: {
+        "dependencies": [
+            "@erfanigh/use-form-handler",
+            "axios",
+            "next",
+            "react",
+            "react-dom",
+            "react-icons",
+            "yup",
+            "zustand",
+            "commander"
+        ],
+        "devDependencies": [
+            "@types/react",
+            "@types/react-dom",
+            "@typescript-eslint/eslint-plugin",
+            "@typescript-eslint/parser",
+            "eslint",
+            "eslint-plugin-react-hooks",
+            "eslint-plugin-react-refresh",
+            "typescript",
+        ]
+    }, 
+    vue: {
+        "dependencies": [
+            "axios",
+            "oh-vue-icons",
+            "pinia",
+            "vue",
+            "yup",
+        ],
+        "devDependencies": [
+            "@vitejs/plugin-vue",
+            "typescript",
+            "vite",
+            "vue-tsc",
+        ]
     },
-    "devDependencies": {
-      "@types/node": "^20.10.0"
+    react: {
+        "dependencies": [
+            "zustand",
+            "react",
+            "react-dom",
+            "react-router-dom",
+        ],
+        "devDependencies": [
+            "@types/react",
+            "@types/react-dom",
+            "@typescript-eslint/eslint-plugin",
+            "@typescript-eslint/parser",
+            "@vitejs/plugin-react",
+            "eslint",
+            "eslint-plugin-react-hooks",
+            "eslint-plugin-react-refresh",
+            "typescript",
+            "vite",
+        ]
+    },
+    express: {
+        "dependencies": [
+            "cookie-parser",
+            "express",
+            "morgan",
+            "cors",
+            "jsonwebtoken",
+            "http-status-codes",
+            "formidable",
+            "yup",
+            "drizzle-orm",
+            "md5",
+            "postgres",
+            ...swcPackageList
+        ],
+        "devDependencies": [
+            "nodemon",
+            "@types/express",
+            "@types/formidable",
+        ]
+    },
+    adminjs: {
+        "dependencies": [
+            "adminjs",
+            "@adminjs/express",
+            "express",
+            "express-formidable",
+            "express-session",
+            "@adminjs/sequelize",
+            "sequelize",
+            "pg",
+            ...swcPackageList
+        ],
+        "devDependencies": [
+            "@typescript-eslint/eslint-plugin",
+            "@typescript-eslint/parser",
+            "dotenv-cli",
+            "eslint",
+            "eslint-config-airbnb",
+            "eslint-plugin-import",
+            "eslint-plugin-jsx-a11y",
+            "eslint-plugin-prettier",
+            "eslint-plugin-react",
+            "eslint-plugin-react-hooks",
+            "prettier",
+            "typescript",
+            "@types/express",
+            "tslib",
+            "sequelize-cli",
+        ]
+    },
+    "telegram-bot": {
+        "dependencies": [
+            "axios",
+            "telegraf",
+        ],
+        "devDependencies": [
+            "@telegraf/types",
+        ]
+    }, 
+    base: {
+        "dependencies": [
+            "dotenv"
+        ],
+        "devDependencies": [
+            "@types/node"
+        ]
     }
 }
 
 const configs = {
     frontend: {
         // TODO add css:build to all frontend project
-        _global: {},
-        next: {
+        _global: async () => ({
             "scripts": {
-                "client:dev": "cd client && npm run changeBaseurl && next dev",
-                "client:build": "cd client && npm run changeBaseurl -- --nodeenv production && next build",
-                "client:start": "cd client && npm run changeBaseurl -- --nodeenv production && next",
                 "changeBaseurl": "node client/scripts/changeBaseurl.js",
-                "css:build": "cd client && npx tailwindcss -i src/assets/styles/index.css -o src/assets/styles/tailwind.css -w"
+                "css:build": "npx tailwindcss -i src/assets/styles/index.css -o src/assets/styles/tailwind.css -w"
             },
-            "dependencies": {
-                "@erfangharib/use-form-handler": "^1.0.1",
-                "axios": "^1.6.8",
-                "next": "^14.2.3",
-                "react": "^18.2.0",
-                "react-dom": "^18.2.0",
-                "react-icons": "^5.2.1",
-                "yup": "^1.4.0",
-                "zustand": "^4.5.2"
+        }),
+        next: async () => ({
+            "scripts": {
+                "client:dev": "npm run changeBaseurl && next dev",
+                "client:build": "npm run changeBaseurl -- --nodeenv production && next build",
+                "client:start": "npm run changeBaseurl -- --nodeenv production && next",
             },
-            "devDependencies": {
-                "@types/react": "^18.2.66",
-                "@types/react-dom": "^18.2.22",
-                "@typescript-eslint/eslint-plugin": "^7.2.0",
-                "@typescript-eslint/parser": "^7.2.0",
-                "eslint": "^8.57.0",
-                "eslint-plugin-react-hooks": "^4.6.0",
-                "eslint-plugin-react-refresh": "^0.4.6",
-                "typescript": "^5.2.2"
-            }
-        },
-        vue: {
+            "dependencies": await configDependencies("next"),
+            "devDependencies": await configDependencies("next", true)
+        }),
+        vue: async () => ({
             "scripts": {
                 "client:start": "cd client && vite",
                 "client:build": "npm run changeBaseurl -- --nodeenv production && cd client && vite build",
                 "css:build": "cd client && npx tailwindcss -i src/assets/styles/index.css -o src/assets/styles/tailwind.css -w",
                 "changeBaseurl": "node client/scripts/changeBaseurl.js"
             },
-            "dependencies": {
-                "axios": "^1.6.8",
-                "dotenv": "^16.3.1",
-                "oh-vue-icons": "^1.0.0-rc3",
-                "pinia": "^2.1.7",
-                "vue": "^3.4.21",
-                "yup": "^1.4.0"
-            },
-            "devDependencies": {
-                "@types/node": "^20.10.0",
-                "@vitejs/plugin-vue": "^5.0.4",
-                "typescript": "^5.2.2",
-                "vite": "^5.2.0",
-                "vue-tsc": "^2.0.6"
-            }
-        },
-        react: {
+            "dependencies": await configDependencies("vue"),
+            "devDependencies": await configDependencies("vue", true),
+        }),
+        react: async () => ({
             "scripts": {
                 "client:start:w": "vite",
                 "client:start": "vite preview",
                 "client:build": "tsc && vite build",
                 "lint": "eslint . --ext ts,tsx --report-unused-disable-directives --max-warnings 0",
             },
-            "dependencies": {
-                "zustand": "^4.5.2",
-                "react": "^18.2.0",
-                "react-dom": "^18.2.0",
-                "react-router-dom": "^6.23.0"
-            },
-            "devDependencies": {
-                "@types/react": "^18.2.66",
-                "@types/react-dom": "^18.2.22",
-                "@typescript-eslint/eslint-plugin": "^7.2.0",
-                "@typescript-eslint/parser": "^7.2.0",
-                "@vitejs/plugin-react": "^4.2.1",
-                "eslint": "^8.57.0",
-                "eslint-plugin-react-hooks": "^4.6.0",
-                "eslint-plugin-react-refresh": "^0.4.6",
-                "typescript": "^5.2.2",
-                "vite": "^5.2.0"
-            }
-        },
+            "dependencies": await configDependencies("react"),
+            "devDependencies": await configDependencies("react", true),
+        }),
     },
     backend: {
-        _global: {},
-        express: {
+        _global: async () => ({}),
+        express: async () => ({
             "scripts": {
-                "api:start": "node dist/index",
-                "api:build": "swc-build ./",
-                "api:build:w": "swc-build --watch ./",
-                "api:start:w": "nodemon --exec \"node dist/index\""
+                "api:start": "node dist/src/index",
+                "api:build": "swc src -d dist ./",
+                "api:build:w": "swc src -d dist -w ./",
+                "api:start:w": "nodemon --exec \"node dist/src/index\""
             },
-            "dependencies": {
-                "swc-build": "^1.0.0",
-                "nodemon": "^3.1.3"
-            },
-        },
-        adminjs: {
+            "dependencies": await configDependencies("express"),
+            "devDependencies": await configDependencies("express", true),
+        }),
+        adminjs: async () => ({
             "scripts": {
-                "adminjs:build": "tsc",
-                "adminjs:build:w": "swc-build --watch ./",
+                "adminjs:build": "swc src -d dist ./",
+                "adminjs:build:w": "swc src -d dist -w ./",
                 "adminjs:start": "dotenv -c '.env' -- node dist/app",
                 "lint": "eslint \"src\"",
             },
-            "dependencies": {
-                "swc-build": "^1.0.0",
-                "adminjs": "^7.4.1",
-                "@adminjs/express": "^6.1.0",
-                "express": "^4.18.2",
-                "express-formidable": "^1.2.0",
-                "express-session": "^1.17.3",
-                "@adminjs/sequelize": "^4.0.0",
-                "sequelize": ">=6",
-                "pg": "latest"
-            },
-            "devDependencies": {
-                "@types/node": "^18.15.11",
-                "@typescript-eslint/eslint-plugin": "^5.58.0",
-                "@typescript-eslint/parser": "^5.58.0",
-                "dotenv-cli": "^7.2.1",
-                "eslint": "^8.38.0",
-                "eslint-config-airbnb": "^19.0.4",
-                "eslint-plugin-import": "^2.27.5",
-                "eslint-plugin-jsx-a11y": "^6.7.1",
-                "eslint-plugin-prettier": "^5.0.1",
-                "eslint-plugin-react": "^7.32.2",
-                "eslint-plugin-react-hooks": "^4.6.0",
-                "prettier": "^3.1.0",
-                "typescript": "^5.0.4",
-                "@types/express": "^4.17.17",
-                "tslib": "^2.5.0",
-                "sequelize-cli": "^6.6.2"
-            }
-        },
+            "dependencies": await configDependencies("adminjs"),
+            "devDependencies": await configDependencies("adminjs", true),
+        }),
     },
     'nodejs-app': {
-        _global: {
+        _global: async () => ({
             "scripts": {
-                "build": "swc-build ./",
-                "build:w": "swc-build --watch ./",
+                "build": "swc src -d dist ./",
+                "build:w": "swc src -d dist -w ./",
                 "start": "dotenv -c '.env' -- node dist/src/index.js",
                 "start:w": "set NODE_ENV=development&& nodemon dist/src/index.js"
             },
-        }
+        })
     },
     'telegram-bot': {
-        _global: {
+        _global: async () => ({
             "scripts": {
-                "bot:build": "swc-build ./",
-                "bot:build:w": "swc-build --watch ./bot",
+                "bot:build": "swc src -d dist ./",
+                "bot:build:w": "swc src -d dist -w ./bot",
                 "bot:start": "dotenv -c '.env' -- node dist/src/index.js",
                 "bot:dev": "set NODE_ENV=development&& nodemon dist/src/index.js"
             },
-            "dependencies": {
-                "axios": "^1.6.2",
-                "dotenv": "^16.3.1",
-                "telegraf": "^4.15.3",
-            },
-            "devDependencies": {
-                "@telegraf/types": "^7.1.0",
-                "@types/node": "^20.10.0",
-            }
-        },
+            "dependencies": await configDependencies("telegram-bot"),
+            "devDependencies": await configDependencies("telegram-bot", true),
+        }),
         'no-admin-panel': {},
         'with-admin-panel': {},
     }
 }
 
-const packageJson = (projectName, projectType, techName) => {
+const getAlreadyCreatedPackageJson = (projectName) => {
+    try {
+        return JSON.parse(
+            fs.readFileSync(
+                path.join(process.cwd(), projectName, 'package.json')
+            )
+        );
+    } 
+    catch(err) {
+        return null;
+    }
+}
+
+// get packages list and fetch latest version number of package
+const configDependencies = async (name, isDev = false) => {
+    const dependencyMode = isDev ? "devDependencies" : "dependencies";
+    const currentTechDependencies = dependencies[name];
+    let result = {};
+
+    if(!currentTechDependencies)
+        throw new Error(`invalid tech name: ${name}`);
+
+    for (let index = 0; index < currentTechDependencies[dependencyMode].length; index++) {
+        const element = currentTechDependencies[dependencyMode][index];
+        const data = (await fetch(`https://registry.npmjs.org/${element}/latest`)).json();
+        const version = '^' + (await data)?.version;
+    
+        result[element] = version;
+    }
+
+    return result;
+}
+
+function deepMerge(target, source) {
+    for (const key in source) {
+        if (Object.hasOwnProperty.call(source, key)) {
+            target[key] = target[key] ?? {};
+
+            if(typeof source[key] === 'object') 
+                Object.assign(target[key], source[key]);
+            else 
+                target[key] = source[key]
+        }
+    }
+}
+
+const packageJson = async (projectName, projectType, techName) => {
     const currentProjectType = configs[projectType];
-    const currentProjectTech = currentProjectType?.[techName] ?? '';
+    const currentProjectTech = await currentProjectType?.[techName]() ?? {};
 
     if(!currentProjectType)
         throw new Error(`no project type exist with name \`${projectType}\``);
 
-    Object.assign(currentProjectType?.['_global'], currentProjectTech)
-    Object.assign(base, currentProjectType?.['_global']);
+    let base = getAlreadyCreatedPackageJson(projectName) ?? {
+        "name": projectName,
+        "type": "module",
+    }
+
+    deepMerge(currentProjectTech, await currentProjectType?.['_global']())
+    deepMerge(base, currentProjectTech);
+    deepMerge(base, {
+        "dependencies": await configDependencies("base"),
+        "devDependencies": await configDependencies("base", true)
+    });
 
     return {
         data: JSON.stringify(base, null, 4),
